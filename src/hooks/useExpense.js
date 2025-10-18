93b9edf5d793898a28d1b491507905c079c813a3
Mikaela Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2025-10-18 18:29:50 +0800

    feat(expenses): implement update and delete functions

--- src/hooks/useExpense.js
+++ src/hooks/useExpense.js
@@ -15,11 +15,39 @@ export const useExpenses = () => {
         setExpenses((prev) => [newExpense, ...prev])
     }
 
-    
+    const updateExpense = (id, updatedExpense) => {
+      setExpenses((prev) =>
+        prev.map((expense) =>
+          expense.id === id
+            ? {
+                ...expense,
+                title: updatedExpense.title.trim(),
+                description: updatedExpense.description?.trim() || "",
+                amount: parseFloat(updatedExpense.amount),
+                date: updatedExpense.date,
+              }
+            : expense
+        )
+      );
+    };
 
 
+    const deleteExpense = (id) => {
+    setExpenses((prev) => prev.filter((expense) => expense.id !== id));
+    };
 
+    const totalExpenses = expenses.reduce(
+    (sum, expense) => sum + expense.amount,
+    0
+    );
 
-
+    return {
+        expenses,
+        addExpense,
+        updateExpense,
+        deleteExpense,
+        totalExpenses,
+        isLoaded
+    };
 
 }
